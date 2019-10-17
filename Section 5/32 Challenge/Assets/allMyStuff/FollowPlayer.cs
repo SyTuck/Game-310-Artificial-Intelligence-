e01b2d88fe78a150cae64cd0f233f22e06b3cce8
JORDAN TUCK [Student] <jordan.tuck@sl.on.ca>
2019-10-17 11:59:38 -0400

    32 completed challenge

---
+++ Section 5/32 Challenge/Assets/allMyStuff/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    private NavMeshAgent thisNavAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisNavAgent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisNavAgent.destination = player.transform.position;
+    }
+}
