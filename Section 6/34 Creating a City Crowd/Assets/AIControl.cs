16cb08038765e7248609ab6f889eaba3a2d764e4
SyTuck <jptuck@yahoo.com>
2019-10-19 20:47:33 -0400

    City crowd and fleeing

--- Section 6/34 Creating a City Crowd/Assets/AIControl.cs
+++ Section 6/34 Creating a City Crowd/Assets/AIControl.cs
@@ -10,18 +10,63 @@ public class AIControl : MonoBehaviour {
 
     Animator anim;
 
+    float so;
+    float detectionRadius = 5;
+    float fleeRadius = 10;
 
-	// Use this for initialization
-	void Start () {
+    void ResetAgent()
+    {
+        so = Random.Range(0.85f, 1.15f);
+        anim.SetFloat("sOffset", so);
+        anim.speed *= so;
+        agent.angularSpeed = 120;
+        anim.SetTrigger("isWalking");
+        agent.ResetPath();
+    }
+
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if (Vector3.Distance(position, this.transform.position) < detectionRadius)
+        {
+            Vector3 fleeDirection = (this.transform.position - position).normalized;
+            Vector3 newgoal = this.transform.position + fleeDirection * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newgoal, path);
+
+            if (path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10;
+                agent.angularSpeed = 500;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
 		goalLocations = GameObject.FindGameObjectsWithTag("goal");
 		agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
 		agent.SetDestination(goalLocations[Random.Range(0,goalLocations.Length)].transform.position);
         anim = this.GetComponent<Animator>();
+        anim.SetFloat("wOffset", Random.Range(0, 1));
         anim.SetTrigger("isWalking");
-	}
+        ResetAgent();
+
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if (agent.remainingDistance < 2)
+        {
+            ResetAgent();
+            agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        }
 	}
+
+
 }
---
+++ Section 6/34 Creating a City Crowd/Assets/Prefabs/rngGoal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rngGoal : MonoBehaviour
+{
+    public Transform minCorner;
+    public Transform maxCorner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.transform.position = new Vector3(Random.Range(minCorner.position.x, maxCorner.position.x),
+                                                                     (minCorner.position.y + minCorner.position.y)/2,
+                                                         Random.Range(minCorner.position.z, maxCorner.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
